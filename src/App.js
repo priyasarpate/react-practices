c841cda40bd647b3b819f936f83ea12d29a52a58
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-05-05 17:10:44 +0530

    cleaning all files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <div>heelo </div>;
 }
 
-export default App
+export default App;
